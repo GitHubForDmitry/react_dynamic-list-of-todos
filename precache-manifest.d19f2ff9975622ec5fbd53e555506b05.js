9cb63140cd8bf4079a76d41a65e64af8504fb13a
GitHubForDmitry <dmitrykoka@gmail.com>
2019-07-08 19:37:47 +0300

    Updates

---
+++ precache-manifest.d19f2ff9975622ec5fbd53e555506b05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f607ab8c27a0fe6bf19d53db3b163c4",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "b21db92d1f91296cd46a",
+    "url": "/react_dynamic-list-of-todos/static/css/main.00b0e363.chunk.css"
+  },
+  {
+    "revision": "e1365f145b5bb8e93d65",
+    "url": "/react_dynamic-list-of-todos/static/js/2.01a65332.chunk.js"
+  },
+  {
+    "revision": "b21db92d1f91296cd46a",
+    "url": "/react_dynamic-list-of-todos/static/js/main.d37c81c4.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
